63f1e844d96e34c2ae30926e4d74732c38c2c7e2
anjaligithub753 <dhaked753@gmail.com>
2024-12-25 14:04:01 +0530

    Fix : Playstore icon small

--- src/Component/LandingPageComponents/two/Playstore.js
+++ src/Component/LandingPageComponents/two/Playstore.js
@@ -1,12 +1,12 @@
 import { GooglePlayButton } from "react-mobile-app-button";
-    import './Playstore.css';
+
 export const Playstore = () => {
   const APKUrl = "https://play.google.com/store/apps/details?id=com.loanunbox.realscore&hl=en";
 
   return (
     <div className="play">
       <GooglePlayButton
-      height={60}
+      height={55}
       width={250}
         url={APKUrl}
         theme={"light"}
