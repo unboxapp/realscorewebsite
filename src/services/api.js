1bd82364a3678ad348ede09adf93a574691de23a
unboxapp <loanunboxapp@gmail.com>
2025-01-04 14:11:59 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://api.realscore.in:3000';
+const API_BASE_URL = 'https://realscorewebsite-backend.onrender.com/';
 
 export const createOrder = async (orderData) => {
   try {
