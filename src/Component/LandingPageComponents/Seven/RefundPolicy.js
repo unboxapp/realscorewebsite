991eff5e5f269ff9a1339c1b143c1102ebf2e850
rajatgrover27 <rajatjayeshgrover@gmail.com>
2025-01-31 10:24:18 +0530

    formatting for device compatibility

--- src/Component/LandingPageComponents/Seven/RefundPolicy.js
+++ src/Component/LandingPageComponents/Seven/RefundPolicy.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import './Terms3.css'; // Extract styles to an external CSS file for better maintainability.
 import SeventhComponent from './SeventhComponent';
+import LogoRealScore from './assets/RealScoreLogo.webp';
 
 const RefundPolicy = () => {
     return (
         <div className='outer-ref'>
             {/* Navigation section */}
             <div className="nav-term">
-                <div className="logo">
-                    <img src="../image/RealScoreLogo.png" alt="RealScore Logo" height="100px" />
-                </div>
+                <img src={LogoRealScore} alt="Logo" height="40px" />
             </div>
 
             {/* Content section */}
@@ -20,17 +19,17 @@ const RefundPolicy = () => {
                 </b>
                 <p>
                     LOANUNBOX / RealScore believes in helping its customers as far as possible, and has therefore a liberal cancellation policy. Under this policy:
-                    <br />
+                   
                     Cancellations will be considered only if the request is made immediately after placing the order. However, the cancellation request may not be entertained if the orders have been communicated to the vendors/merchants and they have initiated the process of shipping them.
-                    <br />
+                   
                     LOANUNBOX / Real Score does not accept cancellation requests for perishable items like flowers, eatables, etc. However, refund/replacement can be made if the customer establishes that the quality of the product delivered is not good.
-                    <br />
+                   
                     In case of receipt of damaged or defective items, please report the same to our Customer Service team. The request will, however, be entertained once the merchant has checked and determined the same at his own end. This should be reported within 30+ days of receipt of the products.
-                    <br />
+                   
                     In case you feel that the product received is not as shown on the site or as per your expectations, you must bring it to the notice of our customer service within 30+ days of receiving the product. The Customer Service Team, after looking into your complaint, will take an appropriate decision.
-                    <br />
+                   
                     In case of complaints regarding products that come with a warranty from manufacturers, please refer the issue to them.
-                    <br />
+                   
                     In case of any Refunds approved by the LOANUNBOX / RealScore, it’ll take 3-5 days for the refund to be processed to the end customer.
                 </p>
             </div>
